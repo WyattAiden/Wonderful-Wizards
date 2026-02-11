857e67af6e45dc7bbc5d583c4008c7a7b0ea1f2c
WyattAiden <wyatt.sobota@mohawkcollege.ca>
2026-02-11 14:42:53 -0500

    AudioManager added

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    AudioSource Music;
-    AudioSource SFX;
+    [Header("------- Audio Source -------")]
+    [SerializeField] AudioSource Music;
+    [SerializeField] AudioSource SFX;
+    [Header("------- Audio Clips -------")]
+    public AudioClip GrvitySwitch;
+    public AudioClip pickupleft;
+    public AudioClip pickupright;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()

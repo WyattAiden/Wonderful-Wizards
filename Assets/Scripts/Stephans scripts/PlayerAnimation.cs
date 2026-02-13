80767c0cb363a3006ea16562b7f4fd3a96659bdd
Stephan <wearshark13@gmail.com>
2026-02-13 14:15:05 -0500

    player animations fixed

---
+++ Assets/Scripts/Stephans scripts/PlayerAnimation.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    private PlayerController controller;
+
+    float moveTimer;
+    public float timeToRun = 0.7f;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        controller = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        float speed = Mathf.Abs(rb.linearVelocity.x);
+
+        anim.SetFloat("Speed", speed);
+        anim.SetFloat("YVelocity", rb.linearVelocity.y);
+        anim.SetBool("IsGrounded", controller.isGrounded);
+
+        // run delay logic
+        if (speed > 0.1f && controller.isGrounded)
+            moveTimer += Time.deltaTime;
+        else
+            moveTimer = 0f;
+
+        anim.SetBool("IsRunning", moveTimer >= timeToRun);
+    }
+}
+
